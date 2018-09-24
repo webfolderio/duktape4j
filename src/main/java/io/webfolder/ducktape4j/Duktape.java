62bd390dda7e3cbeb58e6fb14e0792904dfb260e
webfolderio <support@webfolder.io>
2018-09-24 10:39:49 +0300

    Fix broken build

--- src/main/java/io/webfolder/ducktape4j/Duktape.java
+++ src/main/java/io/webfolder/ducktape4j/Duktape.java
@@ -36,13 +36,18 @@ import java.util.logging.Logger;
 public final class Duktape implements Closeable {
 
     static {
-        String os = getProperty("os.name").toLowerCase(ENGLISH);
-        boolean win = os.contains("windows");
-        if (win) {
-            loadLibrary("META-INF/duktape.dll");
-        } else {
-            loadLibrary("META-INF/duktape.so");
-        }
+    	String path = getProperty("duktape4j.library.path");
+    	if ( path != null && ! path.trim().isEmpty() ) {
+    		load(path);
+    	} else {
+            String os = getProperty("os.name").toLowerCase(ENGLISH);
+            boolean win = os.contains("windows");
+            if (win) {
+                loadLibrary("META-INF/duktape.dll");
+            } else {
+                loadLibrary("META-INF/duktape.so");
+            }
+    	}
     }
 
   /**
